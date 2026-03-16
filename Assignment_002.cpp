b1ea30c7251881069033b6d244a12f2fecbe8c2d
Mikylla Delos Reyes <ty202500691@wmsu.edu.ph>
2026-03-16 10:17:31 +0800

    Add files via upload

---
+++ Assignment_002.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+using namespace std;
+
+void analyzeGrades(int* grades, int students, int subjects){
+
+    cout << "\n============= GRADE MATRIX =======2======\n";
+    cout << "\t";
+    for(int j=0; j<subjects; j++)
+        cout << "SUB" << j+1 << "\t";
+    cout << "AVERAGE";
+    cout << "\n";
+
+    for(int i=0; i<students; i++){
+        cout << "S" << i+1 << "\t";
+        int sum = 0;
+        for(int j=0; j<subjects; j++){
+            cout << *(grades + i*subjects + j) << "\t";
+            sum += *(grades + i*subjects + j);
+        }
+        double avg = (double)sum / subjects;
+        cout << avg;
+        cout << "\n";
+    }
+
+    int highest = *(grades);
+    for(int i=0; i<students; i++)
+        for(int j=0; j<subjects; j++)
+            if(*(grades + i*subjects + j) > highest)
+                highest = *(grades + i*subjects + j);
+
+    cout << "\nHIGHEST GRADE: " << highest << endl;
+}
+
+int main(){
+    int students, subjects;
+    cout << "ENTER NUMBER OF STUDENTS: ";
+    cin >> students;
+    cout << "ENTER NUMBER OF SUBJECTS: ";
+    cin >> subjects;
+
+    int grades[students][subjects];
+
+    for(int i=0; i<students; i++)
+        for(int j=0; j<subjects; j++){
+            cout << "STUDENT " << i+1 << " SUBJECT " << j+1 << ": ";
+            cin >> grades[i][j];
+        }
+
+    analyzeGrades(&grades[0][0], students, subjects);
+
+    return 0;
+}
