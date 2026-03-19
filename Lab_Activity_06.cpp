5edb01b43a41144e7a27a4beafcb4656bba3d8c1
Mikylla Delos Reyes <ty202500691@wmsu.edu.ph>
2026-03-19 16:05:17 +0800

    Add files via upload

---
+++ Lab_Activity_06.cpp
@@ -0,0 +1,93 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+struct book {
+    int barcode;
+    string title;
+    int year;
+    int rating;
+};
+
+string getCategory(int rating){
+    if(rating == 5) return "EXCELLENT";
+    else if(rating == 4) return "VERY GOOD";
+    else if(rating == 3) return "GOOD";
+    else if(rating == 2) return "FAIR";
+    else if(rating == 1) return "POOR";
+    else                 return "NO RATING";
+}
+
+int main(){
+    char runAgain;
+
+    do {
+        system("cls");
+        int numBooks;
+        cout << "ENTER NUMBER OF BOOKS: ";
+        cin >> numBooks;
+
+        book books[numBooks];
+
+        for(int i=0; i<numBooks; i++){
+            cout << "\n=== BOOK " << i+1 << " ===\n";
+
+            int barcode;
+            bool duplicate;
+            do {
+                duplicate = false;
+                cout << "BARCODE: ";
+                cin >> barcode;
+
+                for(int k=0; k<i; k++){
+                    if(books[k].barcode == barcode){
+                        cout << "BARCODE ALREADY EXISTS! ENTER ANOTHER.\n";
+                        duplicate = true;
+                        break;
+                    }
+                }
+            } while(duplicate);
+
+            books[i].barcode = barcode;
+
+            cout << "TITLE:          ";
+            cin.ignore();
+            getline(cin, books[i].title);
+
+            cout << "YEAR PUBLISHED: ";
+            cin >> books[i].year;
+
+            do {
+                cout << "RATING (0-5):   ";
+                cin >> books[i].rating;
+                if(books[i].rating < 0 || books[i].rating > 5)
+                    cout << "INVALID RATING!!! ENTER 0-5 ONLY.\n";
+            } while(books[i].rating < 0 || books[i].rating > 5);
+
+        }
+
+        cout << "\n========================================";
+        cout << "==========================================\n";
+        cout << "Barcode\t\tTitle\t\t\tYEAR\tRATING\tCATEGORY\n";
+        cout << "========================================";
+        cout << "==========================================\n";
+
+        for(int i=0; i<numBooks; i++){
+            cout << books[i].barcode << "\t\t"
+                 << books[i].title   << "\t\t\t"
+                 << books[i].year    << "\t"
+                 << books[i].rating  << "\t"
+                 << getCategory(books[i].rating)<< "\n";
+        }
+
+        cout << "========================================";
+        cout << "==========================================\n";
+
+        cout << "\nDO YOU WANT TO RUN AGAIN? (y/n): ";
+        cin >> runAgain;
+
+    } while(runAgain == 'y' || runAgain == 'Y');
+
+    cout << "\nDAGHANG SALAMAT!!!" << endl;
+    return 0;
+}
